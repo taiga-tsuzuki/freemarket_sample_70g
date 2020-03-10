d7972b2954a51f98ed6a91ae3800bb29bfde2402
kenkoba-valks <kenkoba1231@gmail.com>
2020-03-10 19:04:44 +0900

    change_validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  validates
   has_many :items, dependent: :destroy
   has_many :comments
   has_one :profile
