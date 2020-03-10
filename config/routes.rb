037f58a47200270ab868bb5d0462b057aa656962
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-03-10 17:55:31 +0900

    Update view

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
     root "items#index"
     resources :items do
+      collection do
+        post :confirm
+      end
       resources :comments, only: :create
       resources :images, only: [:index,:create]
+      
     end
     resources :users, only: [:show] do
       resources :profile, only: :index
