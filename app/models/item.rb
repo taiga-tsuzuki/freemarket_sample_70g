8fc37de7f801665ee57bc0094166501baa149f5d
taiga-tsuzuki <60640899+taiga-tsuzuki@users.noreply.github.com>
2020-03-11 20:23:39 +0900

    item

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,16 +2,15 @@ class Item < ApplicationRecord
   has_many :comments
   has_many :images
   belongs_to :user
-  # belongs_to :category
-  # belongs_to :brand
+  belongs_to :category
+  belongs_to :brand
 
 
 
 # fields_forを使うためのメソッドaccepts_nested_attributes_for
 # allow_destroy: trueで、親(item)が削除されると紐づいているimage,category,brandが消える
   accepts_nested_attributes_for :images, allow_destroy: true
-  # accepts_nested_attributes_for :category, allow_destroy: true
-  # accepts_nested_attributes_for :brand, allow_destroy: true
+  accepts_nested_attributes_for :brand, allow_destroy: true
 
 
   validates :item_name, presence: true
