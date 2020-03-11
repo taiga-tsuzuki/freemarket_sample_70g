739d6cf378e24eac70888707885b3c914b2ccbe6
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-03-11 12:02:55 +0900

    change migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_10_102944) do
+ActiveRecord::Schema.define(version: 2020_03_05_040029) do
 
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -21,8 +21,6 @@ ActiveRecord::Schema.define(version: 2020_03_10_102944) do
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "ancestry"
-    t.index ["ancestry"], name: "index_categories_on_ancestry"
   end
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -94,9 +92,10 @@ ActiveRecord::Schema.define(version: 2020_03_10_102944) do
     t.date "birth_day", null: false
     t.text "introduction"
     t.string "user_image"
-    t.bigint "user_id"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -112,4 +111,5 @@ ActiveRecord::Schema.define(version: 2020_03_10_102944) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "profiles", "users"
 end
