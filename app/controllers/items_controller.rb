759b82af94b42b675c15d02675b29b9d5ed805f7
Mst8827 <misato27.t@gmail.com>
2020-03-11 19:04:36 +0900

    wip_image_data_get

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.select('item_name, price').limit(3).order('id DESC')
+    @items = Item.includes(:user).limit(3).order('created_at DESC')
+    @images = Image.includes(:item).limit(3).order('created_at DESC')
   end
 
   def show
