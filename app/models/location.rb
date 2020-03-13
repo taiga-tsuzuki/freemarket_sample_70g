7a1bff8933151d44fe518a28633de5b599410001
kenkoba-valks <kenkoba1231@gmail.com>
2020-03-13 17:21:29 +0900

    add_validation

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,4 @@
 class Location < ApplicationRecord
   belongs_to :user, optional: true
-  # validates :family_name, :first_name, :family_name_kana, :first_name_kana, presence: true
-  # validates :postal_code, :prefecture, :city, :building_name, :phone_name, presence: true
+  validates :family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :prefecture, :city, :building_name, :phone_name, presence: true
 end
