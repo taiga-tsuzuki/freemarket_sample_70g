6af5a90ffd67709ef7611494ca8285cce81f361e
starkazuura8230 <starkazu8230ura@gmail.com>
2020-03-14 13:23:21 +0900

    item-info-edit-comit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,7 +18,26 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.includes(:user).find(params[:id])
+    @user = User.all.find(params[:id])
   end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+    user = User.find(params[:id])
+    user.update(user_params[:user])
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :price, :description, :category_id, :brand_id, :size, :condition, :shipping_fee_payer, :shipping_location, :shipping_days)
+  end
+
+  def user_params
+    params.require(:item).permit(user:[:nickname])
+  end
+
 
   def destroy
   end
