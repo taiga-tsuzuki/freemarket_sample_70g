8d16e99098996a6dbd819201dd0decfcf0745d41
starkazuura8230 <starkazu8230ura@gmail.com>
2020-03-16 20:00:44 +0900

    item-info-edit-comit2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
 
+before_action :set_item, only: [:edit, :update]
+before_action :set_user, only: [:edit, :update]
+
   def index
     @items = Item.includes(:user).order(:item_purchaser_id,:id=>:desc).limit(3)
   end
@@ -18,18 +21,22 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.includes(:user).find(params[:id])
-    @user = User.all.find(params[:id])
+
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    user = User.find(params[:id])
-    user.update(user_params[:user])
+    @item.update(item_params)
+    @user.update(user_params[:user])
   end
 
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
+  def set_item
+    @item = Item.includes(:user).find(params[:id])
+  end
+
   def item_params
     params.require(:item).permit(:item_name, :price, :description, :category_id, :brand_id, :size, :condition, :shipping_fee_payer, :shipping_location, :shipping_days)
   end
