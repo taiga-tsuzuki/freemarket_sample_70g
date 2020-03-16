07025ca4db06634e6e3abdc19f2e90a74ec64306
taiga-tsuzuki <60640899+taiga-tsuzuki@users.noreply.github.com>
2020-03-16 15:13:11 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2020_03_13_041808) do
 
-  create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "prefecture_id"
-    t.string "city"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.datetime "created_at", null: false
@@ -103,10 +96,9 @@ ActiveRecord::Schema.define(version: 2020_03_13_041808) do
     t.date "birth_day", null: false
     t.text "introduction"
     t.string "user_image"
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -126,5 +118,4 @@ ActiveRecord::Schema.define(version: 2020_03_13_041808) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "profiles", "users"
 end
