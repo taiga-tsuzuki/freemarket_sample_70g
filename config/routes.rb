fa5d19ee1dbb185b531405aabe8c00112d54efb0
taiga-tsuzuki <60640899+taiga-tsuzuki@users.noreply.github.com>
2020-03-16 13:32:41 +0900

    クレカ登録、ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root "items#index"
+  resources :items do
+    collection do
+      get :confirm
+      get :done
+      get :category_children
+      get :category_grandchildren
+    end
+    resources :comments, only: :create
+    resources :images, only: [:index,:create]
+  end
+  resources :users, only: [:show] do
+    resources :profile, only: :index
+    resources :creditcards, only: :index
+    resources :location, only: :index
+  end
   resources :creditcards, only: [:new, :show] do
     collection do
       post 'show', to: 'creditcards#show'
@@ -6,21 +23,4 @@ Rails.application.routes.draw do
       post 'delete', to: 'creditcards#delete'
     end
   end
-    devise_for :users
-    root "items#index"
-    resources :items do
-      collection do
-        get :confirm
-        get :done
-        get :category_children
-        get :category_grandchildren
-      end
-      resources :comments, only: :create
-      resources :images, only: [:index,:create]
-    end
-    resources :users, only: [:show] do
-      resources :profile, only: :index
-      resources :creditcards, only: :index
-      resources :location, only: :index
-    end
 end
