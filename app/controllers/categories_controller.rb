401ba256d148d23dc0f1828d1343daf44e664a55
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-03-19 15:56:18 +0900

    Update categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,6 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @categories = @category.items.page(params[:page]).per(20)
+    @category_children = @category.children
   end
 end
