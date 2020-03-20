6e5f82f417666a3280541e1d393bc3c693c33402
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-03-20 16:11:03 +0900

    Update creditcards_controller.rb

--- app/controllers/creditcards_controller.rb
+++ app/controllers/creditcards_controller.rb
@@ -42,7 +42,7 @@ class CreditcardsController < ApplicationController
     if card.blank?
       redirect_to action: "new" 
     else
-      Payjp.api_key = ENV['PAYJP_PRIVATE_KEY']
+      Payjp.api_key = Rails.application.credentials.PAYJP_PRIVATE_KEY
       customer = Payjp::Customer.retrieve(card.customer_id)
       @default_card_information = customer.cards.retrieve(card.card_id)
     end
