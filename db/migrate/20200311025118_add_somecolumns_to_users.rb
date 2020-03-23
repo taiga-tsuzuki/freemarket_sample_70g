8e88cc765a06df45bfde865d318b7e71f20bded0
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-03-23 19:49:51 +0900

    updateculumn

--- db/migrate/20200311025118_add_somecolumns_to_users.rb
+++ db/migrate/20200311025118_add_somecolumns_to_users.rb
@@ -1,9 +1,8 @@
 class AddSomecolumnsToUsers < ActiveRecord::Migration[5.2]
-  def change
+  def down
     add_column :users, :user_family_name, :string, null: false
     add_column :users, :user_first_name, :string, null: false
     add_column :users, :user_family_name_kana, :string, null: false
     add_column :users, :user_first_name_kana, :string, null: false
-    add_column :users, :birth, :date, null: false
   end
 end
