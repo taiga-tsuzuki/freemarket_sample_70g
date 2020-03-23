74ca38db14171dc6c0e2dbdb513f1c3a64b4f6c1
starkazuura8230 <starkazu8230ura@gmail.com>
2020-03-23 20:32:04 +0900

    db:migrate-comit

--- db/migrate/20200311025118_add_somecolumns_to_users.rb
+++ db/migrate/20200311025118_add_somecolumns_to_users.rb
@@ -4,5 +4,6 @@ class AddSomecolumnsToUsers < ActiveRecord::Migration[5.2]
     add_column :users, :user_first_name, :string, null: false
     add_column :users, :user_family_name_kana, :string, null: false
     add_column :users, :user_first_name_kana, :string, null: false
+    add_column :users, :birth, :date, null: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -75,7 +75,7 @@ ActiveRecord::Schema.define(version: 2020_03_13_041808) do
     t.integer "postal_code", null: false
     t.string "prefecture", null: false
     t.string "city", null: false
-    t.string "building_name", null: false
+    t.integer "building_name", null: false
     t.integer "phone_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -114,6 +114,7 @@ ActiveRecord::Schema.define(version: 2020_03_13_041808) do
     t.string "user_first_name", null: false
     t.string "user_family_name_kana", null: false
     t.string "user_first_name_kana", null: false
+    t.date "birth", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
