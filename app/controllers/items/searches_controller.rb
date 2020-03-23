ca078860df5b71beb8f87f60eb668dc2f27ea347
taiga-tsuzuki <60640899+taiga-tsuzuki@users.noreply.github.com>
2020-03-23 15:18:53 +0900

    controller編集

--- app/controllers/items/searches_controller.rb
+++ app/controllers/items/searches_controller.rb
@@ -1,8 +1,10 @@
 class Items::SearchesController < ApplicationController
   def index
     @items = Item.search(params[:keyword])
-    @a = @items.page(params[:page]).per(20)
+    @page = @items.page(params[:page]).per(20)
     @category = Category.all.order("ancestry ASC").limit(13)
     @parents = Category.where(ancestry:nil)
+    @q = Item.ransack(params[:q])
+    @items_ransack = @q.result(distinct: true)
   end
 end
