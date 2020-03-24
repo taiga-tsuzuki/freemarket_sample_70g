751c35b0741587573a5c6119737eb14b1fb1829c
starkazuura8230 <starkazu8230ura@gmail.com>
2020-03-24 13:32:58 +0900

    test-branch-last-comit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,7 @@ class UsersController < ApplicationController
   end
 
   def profile_update
+    @user.update(user_params)
     @profile.update(profile_params)
     redirect_to user_path
   end
@@ -37,6 +38,7 @@ class UsersController < ApplicationController
   private
 
   def set_profile
+    @user = User.find(params[:id])
     @profile = Profile.find(params[:id])
   end
 
@@ -44,10 +46,15 @@ class UsersController < ApplicationController
     @location = Location.find(params[:id])
   end
 
-  def profile_params
-    params.require(:profile).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :birth_year, :introduction, :user_image)
+  def user_params
+    params.require(:user).permit(:user_family_name, :user_first_name, :user_family_name_kana, :user_first_name_kana, :birth, :introduction, :user_image)
   end
 
+  def profile_params
+    params.require(:profile).permit(:introduction)
+    binding.pry
+  end
+  
   def location_params
     params.require(:location).permit(:family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :prefecture, :city, :building_name, :phone_name)
   end
