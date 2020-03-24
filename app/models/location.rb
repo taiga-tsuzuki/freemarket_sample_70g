8b567867472a8d64646071239802ab5cc83cb569
kenkoba-valks <kenkoba1231@gmail.com>
2020-03-24 18:44:29 +0900

    delete_sentense

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,4 @@
 class Location < ApplicationRecord
   belongs_to :user, optional: true
-  validates :family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :prefecture, :city, :building_name, :phone_name, presence: true
+  validates :family_name, :first_name, :family_name_kana, :first_name_kana, :postal_code, :prefecture, :city, :phone_name, presence: true
 end
