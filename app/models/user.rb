430c068e7707da8e2b70fc72170d6c0716714b40
taiga-tsuzuki <60640899+taiga-tsuzuki@users.noreply.github.com>
2020-03-25 13:51:35 +0900

    コントローラー作成、編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:facebook, :google_oauth2]
 
   validates :user_family_name, :user_first_name, :user_family_name_kana, :user_first_name_kana,:birth, presence: true
   validates :email, uniqueness: true, format: { with: /\A([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+\z/ }
@@ -13,4 +13,5 @@ class User < ApplicationRecord
   has_one :profile
   has_many :creditcards
   has_one :location
+  has_many :sns_credentials
 end
