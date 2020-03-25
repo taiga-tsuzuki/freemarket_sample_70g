51a4c1f2032ffe48326eb7fc82880cd7bec940cb
Yoshi101-web <yoshi.dosanko@gmail.com>
2020-03-25 16:43:14 +0900

    Update migrate

--- db/migrate/20200305035555_create_locations.rb
+++ db/migrate/20200305035555_create_locations.rb
@@ -9,7 +9,7 @@ class CreateLocations < ActiveRecord::Migration[5.2]
       t.integer :postal_code, null: false
       t.string :prefecture, null: false
       t.string :city,null: false
-      t.string :building_name, null: false
+      t.string :building_name
       t.integer :phone_name, null: false
       t.timestamps
     end
