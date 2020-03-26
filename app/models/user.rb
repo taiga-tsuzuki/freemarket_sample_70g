c0f5e407c5ba165f1cb7eeec4755670df36f7050
Mst8827 <misato27.t@gmail.com>
2020-03-26 12:32:55 +0900

    password_confirmation_added

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   validates :user_family_name, :user_first_name, :user_family_name_kana, :user_first_name_kana,:birth, presence: true
   validates :email, uniqueness: true, format: { with: /\A([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+\z/ }
   validates :user_family_name, :user_first_name, :user_family_name_kana, :user_first_name_kana, format: { with: /\A[ぁ-んァ-ヶー一-龠]+\z/ }
+  validates :password, confirmation: true
+
 
   has_many :items, dependent: :destroy
   has_many :comments
