622710fbca1189bc69e1ca8456aad93aaff53c6e
taiga-tsuzuki <60640899+taiga-tsuzuki@users.noreply.github.com>
2020-03-27 12:32:13 +0900

    db電話番号の修正

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,12 @@
 
 ActiveRecord::Schema.define(version: 2020_03_25_044730) do
 
+  create_table "brands", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "ancestry"
@@ -78,7 +84,7 @@ ActiveRecord::Schema.define(version: 2020_03_25_044730) do
     t.string "prefecture", null: false
     t.string "city", null: false
     t.string "building_name"
-    t.integer "phone_name", null: false
+    t.string "phone_name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
